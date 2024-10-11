bb156e416300b4e1d7465f1df5c9ea4a6b85abf5
SWelbeck <swelbeck12@gmail.com>
2024-10-11 14:07:46 -0400

    pushing up local repo

---
+++ server.mjs
@@ -0,0 +1,20 @@
+// SBA 318: Express Server Application - Blog
+//Imports
+import express from "express";
+
+// Create an instance of express
+const app = express();
+let PORT = 3000;
+
+
+// Middleware
+
+// Routes
+
+// Error Handling
+
+
+// Listen
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
